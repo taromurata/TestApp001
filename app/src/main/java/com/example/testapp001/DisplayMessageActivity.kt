1d693c16b56c4966398db5aaadc90419297e2594
mio3030 <mio.11302930@gmail.com>
2020-03-15 11:31:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/testapp001/DisplayMessageActivity.kt
@@ -0,0 +1,27 @@
+package com.example.testapp001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.testapp001.R.layout.activity_display_message
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_display_message)
+    }
+}
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(activity_display_message)
+
+    // Get the Intent that started this activity and extract the string
+    val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+    // Capture the layout's TextView and set the string as its text
+    val textView = findViewById<TextView>(R.id.textView).apply {
+        text = message
+    }
+}
+
---
+++ app/src/main/java/com/example/testapp001/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.testapp001
+
+import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
+import android.widget.EditText
+import android.os.Bundle as Bundle
+import android.view.View as View
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button  */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+}
+
+const val EXTRA_MESSAGE = "com.example.testapp001.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // Do something in response to button
+    }
+}
+
+const val EXTRA_MESSAGE = "com.example.testapp001.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.edittext)
+        val message = editText.run { text.toString() }
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
+
