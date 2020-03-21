ef0ae60cbbbf1c66e3ad264a3939e22a03d9afbe
Taro Murata <taro.murata.0526@icloud.com>
2020-03-21 10:05:48 +0900

    Remove duplicate codes.

--- app/src/main/java/com/example/testapp001/MainActivity.kt
+++ app/src/main/java/com/example/testapp001/MainActivity.kt
@@ -7,19 +7,6 @@ import android.widget.EditText
 import android.os.Bundle as Bundle
 import android.view.View as View
 
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    /** Called when the user taps the Send button  */
-    fun sendMessage(view: View) {
-        // Do something in response to button
-    }
-}
-
 const val EXTRA_MESSAGE = "com.example.testapp001.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
@@ -31,25 +18,6 @@ class MainActivity : AppCompatActivity() {
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
         // Do something in response to button
-    }
-}
-
-const val EXTRA_MESSAGE = "com.example.testapp001.MESSAGE"
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    /** Called when the user taps the Send button */
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.edittext)
-        val message = editText.run { text.toString() }
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
     }
 }
 
