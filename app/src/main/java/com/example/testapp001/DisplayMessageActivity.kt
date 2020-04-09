854d9669ed7dd7433187ec98ed6c50c8e6320b15
mio3030 <mio.11302930@gmail.com>
2020-04-09 15:59:48 +0900

    別のアクティビティを追加。

--- app/src/main/java/com/example/testapp001/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/testapp001/DisplayMessageActivity.kt
@@ -8,16 +8,14 @@ import com.example.testapp001.R.layout.activity_display_message
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_display_message)
+        setContentView(R.layout.activity_display_message)
 
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
 
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.editText).apply {
+        val TextView: TextView = findViewById<TextView>(R.id.TextView).apply {
             text = message
         }
-    }
+        }
 }
-
-
